5edb95498304b1f4457dd162e862b0a2a3b02b99
Petar Nenov <petar.nenov-petrov@geowealth.com>
2022-03-17 09:34:01 +0200

    add: logs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ const chalk = require("chalk")
 
 console.log(chalk.blue('Hello world!'));
 console.log(chalk.blue('Hello world! 7'));
+console.log(chalk.blue('Hello world! 1'));
+console.log(chalk.blue('Hello world! 2'));
+console.log(chalk.blue('Hello world! 3'));
